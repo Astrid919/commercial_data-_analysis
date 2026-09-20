cd1de8c37bea4268b615e93c4cb9a2359fd49cff
Astrid919 <jiayichen0919@gmail.com>
2026-09-20 21:52:44 +0800

    Delete analysis directory

--- analysis/05_demand_segmentation.py
+++
@@ -1,6 +0,0 @@
-from common import *
-def main():
-    m,y=load();s=patterns(y);s.insert(0,'SKU',m.SKU);s.insert(1,'INN',m.INN_key);save(s,'demand_segments')
-    inn=aggregate(m,y,'INN_key');a=patterns(inn.to_numpy());a.insert(0,'INN',inn.index);save(a,'inn_demand_segments')
-    save(s.groupby('segment').agg(sku_count=('SKU','size'),mean_zero_rate=('zero_rate','mean'),mean_cv=('CV','mean')).reset_index(),'demand_segment_summary')
-if __name__=='__main__': main()
